3878320e7c08c7e0d4b66ac67398aef2b0b58d9a
egrox <negor3893@gmail.com>
2025-11-09 02:31:13 +0300

    ?

--- Lab_3.py
+++ Lab_3.py
@@ -7,7 +7,7 @@ import random
 
 def generation_key():
     code = ''
-    letters_list = 'abcdefghijklmnopqrstuvwxyz'
+    letters_list = 'abcdefghijklmnopqrstuvwxyz0123456789'
     for i in range(8):
         code += random.choice(letters_list)
     return code
@@ -17,36 +17,56 @@ def clicked():
 
 
     def clic_clac():  
-        res = format(txt.get())  #кнопка ввода начального ключа
+        generation_key_str = generation_key()
+        key_first_line = format(txt.get())  #кнопка ввода начального ключа
+        if (len(key_first_line) != 5):
+            messagebox.showinfo('Error', "Please enter the beginning of the code")
+        else:
+            key_second_line = generation_key_str[:5]
+            key_fird_line = key_first_line[-2:] + generation_key_str[5:]
+            
+            messagebox.showinfo('Key', 
+                                f'''{key_first_line}-
+{key_second_line}-
+{key_fird_line}''')  
+            
 
-        lbl.configure(text=res) 
+    bg_img_2 = tk.PhotoImage(file='zurg.png')
+    label_bg_2 = tk.Label(window, image=bg_img_2)
+    label_bg_2.place(x=0, y=0, relwidth=1, relheight=1)
+    btn_1.place_forget()#убираем первую кнопку
 
+    lbl_1.configure(text='''Enter the beginning
+of the key''')
+    lbl_1.place(relx=0.21, rely=0)
 
-    lbl.configure(text="Я же просил...")
-    
-    txt = Entry(window,width=5)  #текстовое окно
+    txt = Entry(window,width=5, font=("Comic Sans MS", 20), bg="red",
+              fg="yellow")  #текстовое окно
     txt.focus() #фокус покус
-    txt.grid(column=0, row=1) 
+    txt.place(relx=0.25, rely=0.4) 
 
-    btn = Button(window, text="Клик!", command=clic_clac)  #кнопка ввода информации
+    btn_2 = Button(window, text="Клик!", font=("Comic Sans MS", 20), bg="red",
+              fg="yellow", command=clic_clac)  #кнопка ввода информации
 
-    btn.grid(column=1, row=1)  
+    btn_2.place(relx=0.5, rely=0.4) 
 
 
  
 
 
 window = tk.Tk()
-window.geometry('600x520')  
-#bg_img = tk.PhotoImage(file='my_bg_orig.jpg')
-window.title("Добро пожаловать в приложение Goodby_Universiti_Hello_Game")
+window.geometry('400x394')  
+bg_img_1 = tk.PhotoImage(file='my_bg_orig (1).png')
+label_bg_1 = tk.Label(window, image=bg_img_1)
+label_bg_1.place(x=0, y=0, relwidth=1, relheight=1)
+window.title("TOY story 2")
+lbl_1 = Label(window, text="Mysterious key", font=("Comic Sans MS", 20), bg="red",
+              fg="yellow",) 
+lbl_1.place(relx=0.25, rely=0)
 
-lbl = Label(window, text="Привет") 
-lbl.grid(column=0, row=0)
-
-btn = Button(window, text="Не нажимать!", font=("Arial Bold", 25), bg="red",
+btn_1 = Button(window, text="Get code", font=("Comic Sans MS", 25), bg="red",
               fg="yellow", command=clicked)  
-btn.grid(column=1,  row=0)  
+btn_1.place(relx=0.45, rely=0.75) 
 
 
 
